3eb00eb47a40bce2c70f10dc5db2d829c329ff5b
Jagadeesh <jagadeesh.ifs@gmail.com>
2017-03-06 17:49:38 +0530

    first commit

---
+++ MavenTestNGDemo/src/test/java/com/mouniqa/testng/test/TestNgMavenExampleTest.java
@@ -0,0 +1,14 @@
+package com.mouniqa.testng.test;
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenExampleTest {
+
+	@Test
+	public void exampleOfTestNgMaven() {
+
+		System.out.println("This is TestNG-Maven Example");
+
+	}
+
+}
